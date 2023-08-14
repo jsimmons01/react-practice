3959a543cdc494e34c2a411a036f3aa2b99e549c
Jahmella Williams <simmons.jahmella@gmail.com>
2023-08-14 01:28:03 -0400

    Adds useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 
-function Square({ value }){
+function Square(){
+
+  const [value,setValue] = useState(null);
 
   function handleClick(){
-    console.log("click")
+    setValue("X");
   }
 
   return <button className="square" onClick={handleClick}>{value}</button>;
-
  
 }
 
@@ -15,20 +17,20 @@ export default function Board() {
   return (
     <div>
        <div className='board-row'>
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
 
       </div> 
       <div className='board-row'>
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div> 
       <div className='board-row'>
-        <Square value="7"/>
-        <Square value="8" />
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div> 
     </div>
      
