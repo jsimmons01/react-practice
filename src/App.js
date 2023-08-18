91c57e59a8212a1b8153d44fb177af76ec80c7a0
Jahmella Williams <simmons.jahmella@gmail.com>
2023-08-18 00:22:26 -0400

    Updates handleClick

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Square(){
+function Square({value,onSquareClick}){
 
-  const [value,setValue] = useState(null);
 
-  function handleClick(){
-    setValue("X");
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>;
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
  
 }
 
 export default function Board() {
   const [squares,setSquares] = useState(Array(9).fill(null));
+
+  
+
+  function handleClick(){
+    const nextSquares = squares.slice();
+    nextSquares[0] = 'X';
+    setSquares(nextSquares)
+  }
   return (
     <div>
        <div className='board-row'>
-        <Square value={squares[0]}/>
+        <Square value={squares[0]} onSquareClick={handleClick}/>
         <Square value={squares[1]}/>
         <Square  value={squares[2]}/>
 
