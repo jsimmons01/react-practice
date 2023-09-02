38ed49e8a3c7a713e2294e2edbd2b45e1c945500
Jahmella Williams <simmons.jahmella@gmail.com>
2023-09-01 23:44:41 -0400

    Creates winner

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ export default function Board() {
   
 
   function handleClick(i){
-    const nextSquares = squares.slice();
-    if(squares[i]){
+   
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
+
+    const nextSquares = squares.slice();
     if(xIsNext){
       nextSquares[i] = 'X';  
     } else {
@@ -47,10 +49,41 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div> 
     </div>
-     
-
   );
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        // ...
+  )
 }
- 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
   
 
